d7e386ef7908e3c2fc28daec08d9291ea9dbcb28
greenrose7 <lucascevans@live.com>
2025-01-13 16:51:02 -0400

    added function to count for a specific word

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ def main():
     target_book = "books/frankenstein.txt"
     with open(target_book) as f:
         file_contents = f.read()
-        print(file_contents)
+        # print(file_contents)
 
         print(f"--- Begin report of {target_book} ---")
-
         print(f"There are {count_words(file_contents)} words in this text!")
+        print(f"The word Frankenstein is used {count_specific_word(file_contents, "Frankenstein")}")
         print("")
         print("Character breakdowns:")
         character_counts = count_characters(file_contents)
@@ -32,5 +32,16 @@ def count_characters(text):
     
     return character_counts
 
+def count_specific_word(text, word_to_search):
+    word_to_search = word_to_search.lower()
+    text = text.lower()
+    words = text.split()
+    count = 0
+    for i in words:
+        if i == word_to_search:
+            count += 1
+    
+    return count
+    
 
 main()
